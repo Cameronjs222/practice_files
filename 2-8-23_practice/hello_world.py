966fd2edd0dcc8d30f8b8c85597d97fd6454b222
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-02-08 14:33:02 -0800

    hello world assignment

---
+++ 2-8-23_practice/hello_world.py
@@ -0,0 +1,9 @@
+print("Hello world")
+name = "Cameron"
+print(f"hello", name)
+fav_num = 222
+print("Hello " + str(fav_num))
+print("Hello " + str(fav_num))
+fav_foods = ["sushi", "pizza"]
+fav_foods.append("tacos!")
+print(f"I love {fav_foods[0]}, {fav_foods[1]}, and {fav_foods[2]}")
