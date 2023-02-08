182f0c152749e7239890d3e72f890210e8c7fc9f
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-02-08 12:56:21 -0800

    practicing string interperlation

---
+++ string_interperlation.py
@@ -0,0 +1,5 @@
+name = "Cameron"
+profesion = "web developer"
+likes = ["tacos, video games, and dogs"]
+greetings = f"hello my name is {name}, I am a {profesion}, and I like {likes}"
+print(greetings)
