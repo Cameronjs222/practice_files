b40da12516a5be250359e39e3022d33da1b8c9af
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-02-12 01:13:27 -0800

    finished

--- 2-12-23_practice/3sum.js
+++ 2-12-23_practice/3sum.js
@@ -1,18 +1,27 @@
-let nums = [-1,0,1,2,-1,-4]
+let nums = [-1,0,1,2,-1,-4];
+
 var threeSum = function(nums) {
-    output = []
-    for (i=0;i<nums.length;i++){
-        a = 0 - nums[i]
-        for (j=0 && j !== i;j<nums.length;j++){
-            b = a - nums[j]
-            for (k=0 && k !== j;k<nums.length;k++){
-                if (nums[k] + b == 0 && !output.includes(nums[i], nums[j], nums[k])){
-                    output.push(nums[i], nums[j], nums[k])
-                    console.log(output)
+    let output = [];
+    for (i=0;i<nums.length-2;i++){
+        for (j=i+1;j<nums.length-1;j++){
+            for (k=j+1;k<nums.length;k++){
+                if (nums[i] + nums[j] + nums[k] == 0){
+                    let solution = [nums[i], nums[j], nums[k]].sort();
+                    let found = false;
+                    for (let l=0;l<output.length;l++){
+                        if (output[l].toString() == solution.toString()){
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found){
+                        output.push(solution);
+                    }
                 }
             }
         }
     }
-    return output
+    return output;
 };
+
 console.log(threeSum(nums))
