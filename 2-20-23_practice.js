1110fa40d1de6f4d6f30686249e67e5a0045761f
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-02-21 23:08:42 -0800

    need to define iv as 4

--- 2-20-23_practice.js
+++ 2-20-23_practice.js
@@ -1,11 +1,7 @@
-/**
- * @param {string} s
- * @return {number}
- */
 var romanToInt = function(s) {
     var value = 0;
-    var string = s.split("")
-    for (i=0;i<string.length;i++){
+    var string = s.split("") //need to figure out how to define iv as 4
+    for (i=0;i<string.length;i++){ 
         if (string[i] == "i" || string[i] == "I"){
             value += 1
         }
